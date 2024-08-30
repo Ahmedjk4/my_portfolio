9dcd6519b74d176faa188808f919d7e6acab281f
Ahmedjk4 <botabasha1@gmail.com>
2024-08-30 16:47:36 +0300

    updated my projects, web favicon, web loading background color

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -14,7 +14,39 @@ List<ProjectModel> hobbyProjectsList = [
     title: 'Ios Calculator App',
     subtitle:
         'This is an iOS Calculator app for performing basic arithmetic operations.',
-    androidLink: '',
+    androidLink:
+        'https://drive.google.com/drive/folders/11PKtEujRWQKSGDDaC4BVo_B9SRvdlhel?usp=sharing',
+  ),
+  ProjectModel(
+    image: 'assets/projects/bookly.png',
+    title: 'Bookly App',
+    subtitle:
+        'Book Store App for buying and selling books (Actually It Only displays free books for now).',
+    androidLink:
+        'https://drive.google.com/drive/folders/113fqNo_oDM-T22K2MV_ZfZweCBrXd9y3?usp=sharing',
+  ),
+  ProjectModel(
+    image: 'assets/projects/catflix.png',
+    title: 'Catflix App',
+    subtitle:
+        'This is an App for playing some cat games and seeing shorts for cats. (I love Cats)',
+    androidLink:
+        'https://drive.google.com/drive/folders/11mcpkDheRPxnpFDaJ5lLLw62oL1UgZKE?usp=sharing',
+  ),
+  ProjectModel(
+    image: 'assets/projects/todos.png',
+    title: 'ToDo List App',
+    subtitle: 'This is a Todo List App for keeping track of your daily tasks.',
+    androidLink:
+        'https://drive.google.com/drive/folders/10NXQAPd1Lubd8dmolg-0CFmq6abJJzhp?usp=sharing',
+  ),
+  ProjectModel(
+    image: 'assets/projects/weatherapp.png',
+    title: 'Weather App',
+    subtitle:
+        'This is a Weather App for keeping track of your daily weather conditions.',
+    androidLink:
+        'https://drive.google.com/drive/folders/10reao-w_KyhsYl6By8ouZiQTv7SnBSHY?usp=sharing',
   ),
 ];
 
@@ -25,7 +57,6 @@ List<ProjectModel> workProjectsList = [
     image: 'assets/projects/cstore.png',
     title: 'CStore App',
     subtitle:
-        'This is a comprehensive Electronics Store app for buying and selling products.',
-    androidLink: '',
+        'This is a comprehensive Electronics Store app for buying and selling products. (Still Working On it, Link Will be updated soon.)',
   ),
 ];
